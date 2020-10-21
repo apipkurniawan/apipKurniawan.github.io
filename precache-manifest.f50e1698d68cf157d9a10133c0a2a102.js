1cd6d968d697b783ec7ec3914e3c85c5b32f3069
apip <apip@gitlab.lab>
2020-10-22 03:07:40 +0700

    Updates

---
+++ precache-manifest.f50e1698d68cf157d9a10133c0a2a102.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c7564673a4837c201387547e0c373dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d1a64817fbb9c64de11",
+    "url": "/static/css/main.bb9380fb.chunk.css"
+  },
+  {
+    "revision": "7dbc8a248f107db85048",
+    "url": "/static/js/2.ea4c2b7e.chunk.js"
+  },
+  {
+    "revision": "cd6e711b9e263c2324169127d95e8bd1",
+    "url": "/static/js/2.ea4c2b7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d1a64817fbb9c64de11",
+    "url": "/static/js/main.8a00b951.chunk.js"
+  },
+  {
+    "revision": "717a8abd0f249375b28a",
+    "url": "/static/js/runtime-main.0dea4190.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
