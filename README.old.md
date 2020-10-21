a28cbfdd4a630bd60282bd4994f94d60159589f4
apip <apip@gitlab.lab>
2020-10-22 01:05:18 +0700

    create chart

--- README.old.md
+++ README.old.md
@@ -6,4 +6,5 @@ how to build corona tracker app using react
 
 - npx create-react-app ./
 - npm install --save axios react-chartjs-2 react-countup classnames
+- npm install --save chart.js
 - npm install --save @material-ui/core
--- src/api/index.js
+++ src/api/index.js
@@ -14,6 +14,14 @@ export const fetchData = async () => {
 export const fetchedDailyData = async () => {
     try {
         const { data } = await axios.get(url + '/daily');
+
+        const modifyData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate,
+        }));
+
+        return modifyData;
     } catch (error) {
 
     }
