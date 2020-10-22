0b7081e682c3c76e71464ca3611d20f3d6f8354c
apip <apip@gitlab.lab>
2020-10-22 13:09:29 +0700

    Updates

--- precache-manifest.630fe4367b479c46c039ee7aec59045d.js
+++ precache-manifest.630fe4367b479c46c039ee7aec59045d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "15b79dfcc9256df4cbcbda79c5bcc698",
+    "revision": "7460934ebbe4a0f9a05c2e9604061e3f",
     "url": "/corona-tracker/index.html"
   },
   {
-    "revision": "c97bc43f17dc21dd6ebb",
+    "revision": "6c0945c0ca55380116ad",
     "url": "/corona-tracker/static/css/main.1e272e43.chunk.css"
   },
   {
-    "revision": "54ff5e68452b4b682647",
-    "url": "/corona-tracker/static/js/2.bc269f6d.chunk.js"
+    "revision": "82a45f7777ae2dad411d",
+    "url": "/corona-tracker/static/js/2.a1ece58f.chunk.js"
   },
   {
     "revision": "cd6e711b9e263c2324169127d95e8bd1",
-    "url": "/corona-tracker/static/js/2.bc269f6d.chunk.js.LICENSE.txt"
+    "url": "/corona-tracker/static/js/2.a1ece58f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c97bc43f17dc21dd6ebb",
-    "url": "/corona-tracker/static/js/main.c3e9d165.chunk.js"
+    "revision": "6c0945c0ca55380116ad",
+    "url": "/corona-tracker/static/js/main.1c3aed04.chunk.js"
   },
   {
     "revision": "9c4d7b1cf1de7ae99f79",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/corona-tracker/precache-manifest.8ac01c7a59c7872f865266a58d106c9d.js"
+  "/corona-tracker/precache-manifest.630fe4367b479c46c039ee7aec59045d.js"
 );
 
 self.addEventListener('message', (event) => {
